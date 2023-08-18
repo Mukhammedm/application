af943b3590c457620f02c721b64e6ef2ddd70cc4
jdu211171 <211171m@jdu.uz>
2023-08-18 14:21:33 +0500

    slight changes

--- sideBar/script.js
+++ sideBar/script.js
@@ -18,3 +18,26 @@ modeSwitch.addEventListener("click", () => {
     modeText.innerText = "Dark Mode";
   }
 });
+
+/* Nur Islom inserted code */
+// Attach a click event listener to all anchor tags
+// document.addEventListener("DOMContentLoaded", function() {
+//   const spans = document.querySelectorAll("span");
+//   const icons = document.querySelectorAll("i");
+//   const links = spans.concat(icons);
+
+//   links.forEach(link => {
+//     link.addEventListener("click", function(event) {
+//       // Prevent the default link behavior
+//       event.preventDefault();
+
+//       // Remove the clicked-link class from all links
+//       links.forEach(link => {
+//         link.classList.remove("clicked-link");
+//       });
+
+//       // Add the clicked-link class to the clicked link
+//       this.classList.add("clicked-link");
+//     });
+//   });
+// });
